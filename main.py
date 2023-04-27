c5db8d590c7a4c7750dd12733033be254f0c738d
Mustafa Branch <mustafa.branch@gmail.com>
2023-04-26 23:24:07 -0600

    minor change

--- main.py
+++ main.py
@@ -20,18 +20,16 @@ def gpt_35(prompt: str, api: openai):
         "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": f"{query}"}]
     }
-
     
-    response = requests.post(url=endpoint, json=body, headers=headers)
-    js = response.json()
+    response = requests.post(url=endpoint, json=body, headers=headers).json()
+        
+    gpt_response = response["choices"][0]["message"]["content"]
+    print(f"\"{gpt_response}\"") # Send to alfred viewer
+    
     save_path = os.getenv("RESPONSE_PATH")
     
     with open(save_path, "w") as f:
-        json.dump(js, f, indent=4) # Save last response to file
-        
-    gpt_response = js["choices"][0]["message"]["content"]
-    formatted_response = f"\"{gpt_response}\""
-    print(formatted_response) # Send to alfred viewer
+        json.dump(response, f, indent=4) # Save last response to file
     
 if __name__ == "__main__":
     query = sys.argv[1]
